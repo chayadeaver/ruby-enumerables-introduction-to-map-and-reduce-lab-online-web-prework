eb6b831dfabcf8e8e537b3c46600831e89299d30
Chaya Deaver <chayadeaver@gmail.com>
2019-08-06 00:02:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,7 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array)
-  if source_array.all? {|n| n == true}
+  if source_array.reduce {|n| n == true}
   else
     source_array
   end
